afb75273459d78507388fecb156761d1d59c967b
MadhanM282 <madhanm282@gmail.com>
2022-06-22 07:52:56 +0530

    containors

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -3,7 +3,6 @@ import axios from "axios"
 import { useEffect, useState } from "react"
 import { ButtonAppBar } from "./nav"
 import { Card } from "./NewsCard"
-
 export const Home = () => {
     const [Ru, SetRu] = useState([])
     const [In,SetIn] = useState([]);
@@ -11,26 +10,26 @@ export const Home = () => {
     useEffect(() => {
         GetDataIndia()
     }, [])
-    const GetDataRussia = () => {
-        axios.get(`https://newsapi.org/v2/top-headlines?country=nz&apiKey=7731b0203f1f47028f029aab37012d8c`).then(({ data }) => {
-            console.log('data', data);
-            SetRu(data.articles)
-        })
-    }
+    // const GetDataRussia = () => {
+    //     axios.get(`https://newsapi.org/v2/top-headlines?country=nz&apiKey=`).then(({ data }) => {
+    //         console.log('data', data);
+    //         SetRu(data.articles)
+    //     })
+    // }
     const GetDataIndia = () => {
-        axios.get(`https://newsapi.org/v2/top-headlines?country=in&apiKey=7731b0203f1f47028f029aab37012d8c`).then(({ data }) => {
+        axios.get(`https://newsapi.org/v2/top-headlines?country=in&apiKey=${process.env.REACT_APP_APIKEY}`).then(({ data }) => {
             console.log('data', data);
             SetIn(data.articles)
             // GetDataUSA()
         })
     }
-    const GetDataUSA = () => {
-        axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=7731b0203f1f47028f029aab37012d8c`).then(({ data }) => {
-            console.log('data', data);
-            SetUs(data.articles)
-            // GetDataRussia()
-        })
-    }
+    // const GetDataUSA = () => {
+    //     axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=`).then(({ data }) => {
+    //         console.log('data', data);
+    //         SetUs(data.articles)
+    //         // GetDataRussia()
+    //     })
+    // }
 
     return (
         <Box sx={{height:"90vh",overflow:"scroll"}}>
